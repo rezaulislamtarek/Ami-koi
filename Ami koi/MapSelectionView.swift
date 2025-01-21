22ff6cd70cecaafd90cc31bf79ab82eb99bd27bb
rezaulislam <mrezaulislam.t@gmail.com>
2025-01-21 07:06:10 +0600

    Route handler integration

---
+++ Ami koi/MapSelectionView.swift
@@ -0,0 +1,18 @@
+//
+//  MapSelectionView.swift
+//  Ami koi
+//
+//  Created by Rezaul Islam on 21/1/25.
+//
+
+import SwiftUI
+
+struct MapSelectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapSelectionView()
+}
--- Ami koi/Router/RouteHandler.swift
+++ Ami koi/Router/RouteHandler.swift
@@ -5,3 +5,33 @@
 //  Created by Rezaul Islam on 21/1/25.
 //
 
+import SwiftUI
+
+struct RouteHandler : View {
+    @EnvironmentObject private var router : Router
+    var body: some View {
+        NavigationStack(path: $router.navPath) {
+            HomeView()
+                .navigationDestination(for: Route.self) { route in
+                    switch route{
+                    case .homeView : HomeView()
+                    case .reminderListView : Text("Reminders view")
+                    case .setReminderView : SetReminderView()
+                    case .mapSelectionView: MapSelectionView()
+                    }
+                }
+        }
+        .sheet(item: $router.presentedSheet) { item in
+            if let route = item.destination as? Route {
+                switch route {
+                case .mapSelectionView :
+                    MapSelectionView()
+                        .presentationDetents([.medium, .large])
+                        .presentationDragIndicator(.visible)
+                default:
+                    EmptyView()
+                }
+            }
+        }
+    }
+}
