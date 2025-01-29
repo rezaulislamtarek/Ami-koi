56331481e7cdd8ac00640364450e85479f1c8c53
rezaulislamtarek <mrezaulislam.t@gmail.com>
2025-01-29 17:53:12 +0600

    Seperate Quote View from Homeview file

--- Ami koi/Views/HomeView.swift
+++ Ami koi/Views/HomeView.swift
@@ -59,33 +59,3 @@ struct GreetingView: View {
     }
 }
 
-struct QuoteView: View {
-    private var quoteProvider = QuoteProvider()
-    @State private var quoteRenderToggle : Bool = false
-    var body: some View {
-        VStack(spacing : 16){
-            if quoteRenderToggle{
-                Text("\(quoteProvider.getRandomQuote(for: .getGreeting())!)")
-            }else{
-                Text("\(quoteProvider.getRandomQuote(for: .getGreeting())!)")
-            }
-            Image(systemName: "quote.opening")
-        }
-        .foregroundStyle(.white)
-        .multilineTextAlignment(.center)
-        
-        .font(.system(.title2, design: .some(.serif)))
-        .frame(maxWidth: .infinity, alignment: .center)
-        .padding()
-        .padding(.vertical, 44)
-        .background(
-            LinearGradient(colors: [.green.opacity(1), .blue.opacity(0.9)], startPoint: .topLeading, endPoint: .bottomTrailing)
-        )
-        .cornerRadius(24)
-        .onTapGesture {
-            withAnimation {
-                quoteRenderToggle.toggle()
-            }
-        }
-    }
-}
