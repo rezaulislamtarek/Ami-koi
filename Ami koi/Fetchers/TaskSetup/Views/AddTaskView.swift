0c2838a3855e2140c3b0d7e22d9a8c5574b03cd6
rezaulislam <mrezaulislam.t@gmail.com>
2025-01-31 21:23:58 +0600

    refactor view with extension.

--- Ami koi/Fetchers/TaskSetup/Views/AddTaskView.swift
+++ Ami koi/Fetchers/TaskSetup/Views/AddTaskView.swift
@@ -14,48 +14,19 @@ struct AddTaskView: View {
     @EnvironmentObject private var lms : LocationService
     @State private var details : String = ""
     private var corneerRadious : CGFloat = 16
-     
+    
     @State var location : CLLocation? = nil
     
     var body: some View {
-       
+        
         VStack{
-                VStack {
-                   locationPickupButtonSection
-                    
-                    
-                    TextField("Whats should I tell you later?", text: $details)
-                        .padding()
-                        .lineLimit(5)
-                        .frame(minHeight: 120, alignment: .topLeading)
-                        .background(.green.opacity(0.1))
-                        .cornerRadius(corneerRadious)
-                    
-                    Spacer()
-                    
-                    Button {
-                        let task = MyTask(context: context)
-                        task.title = details
-                        task.lat = location?.coordinate.latitude ?? 0
-                        task.lon = location?.coordinate.longitude ?? 0
-                        task.isComplite = false
-                        task.address = lms.address
-                        try? context.save()
-                        lms.addGeofence(for: task)
-                        
-                        
-                    } label: {
-                        Text("Set Reminder")
-                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-                            .padding()
-                            .gradientBackground()
-                            .cornerRadius(corneerRadious)
-                            .foregroundStyle(.white)
-                    }
-
-                        
-                }
-             
+            VStack {
+                locationPickupButtonSection
+                taskDetailsEditorSection
+                Spacer()
+                setTaskButtonSection
+            }
+            
         }
         .padding(32)
         .fontDesign(.serif)
@@ -89,15 +60,46 @@ extension AddTaskView{
                     .foregroundStyle(.primary.opacity(0.7))
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .multilineTextAlignment(.leading)
-                    
+                
             }
             .frame(maxWidth: .infinity, alignment: .leading)
             .foregroundStyle(.white)
             .padding()
             .gradientBackground([.green, .blue.opacity(0.5)])
-        .cornerRadius(corneerRadious)
+            .cornerRadius(corneerRadious)
         })
         .tint(.primary)
+    }
+    
+    private var taskDetailsEditorSection : some View{
+        TextField("Whats should I tell you later?", text: $details)
+            .padding()
+            .lineLimit(5)
+            .frame(minHeight: 120, alignment: .topLeading)
+            .background(.green.opacity(0.1))
+            .cornerRadius(corneerRadious)
+    }
+    
+    private var setTaskButtonSection : some View{
+        Button {
+            let task = MyTask(context: context)
+            task.title = details
+            task.lat = location?.coordinate.latitude ?? 0
+            task.lon = location?.coordinate.longitude ?? 0
+            task.isComplite = false
+            task.address = lms.address
+            try? context.save()
+            lms.addGeofence(for: task)
+            
+            
+        } label: {
+            Text("Set Task")
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                .padding()
+                .gradientBackground()
+                .cornerRadius(corneerRadious)
+                .foregroundStyle(.white)
+        }
     }
 }
 
